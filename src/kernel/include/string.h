57a32e26e5c0bb93e5ccd617681dd85f2f9274d8
friedrichOsDev <f39597054@gmail.com>
2026-01-06 19:21:16 +0100

    Implement string manipulation functions: add strcmp, strcpy, strlen, strnlen, memset, memcpy, and memcmp

---
+++ src/kernel/include/string.h
@@ -0,0 +1,15 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int strcmp(const char* s1, const char* s2);
+char* strcpy(char* dest, const char* src);
+size_t strlen(const char* str);
+size_t strnlen(const char* str, size_t maxlen);
+void* memset(void* ptr, int value, size_t num);
+void* memcpy(void* dest, const void* src, size_t n);
+int memcmp(const void* ptr1, const void* ptr2, size_t num);
+
+#endif // STRING_H
---
+++ src/kernel/lib/string.c
@@ -0,0 +1,57 @@
+#include "../include/string.h"
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
+
+char* strcpy(char* dest, const char* src) {
+    char* original_dest = dest;
+    while ((*dest++ = *src++));
+    return original_dest;
+}
+
+size_t strlen(const char* str) {
+    const char* s;
+    for (s = str; *s; ++s);
+    return (s - str);
+}
+
+size_t strnlen(const char* str, size_t maxlen) {
+    const char* s;
+    for (s = str; *s && maxlen--; ++s);
+    return (s - str);
+}
+
+void* memset(void* ptr, int value, size_t num) {
+    unsigned char* p = ptr;
+    while (num--) {
+        *p++ = (unsigned char)value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = dest;
+    const unsigned char* s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+    const unsigned char* p1 = ptr1;
+    const unsigned char* p2 = ptr2;
+    while (num--) {
+        if (*p1 != *p2) {
+            return *p1 - *p2;
+        }
+        p1++;
+        p2++;
+    }
+    return 0;
+}
