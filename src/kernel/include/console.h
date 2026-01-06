28460932e8db23dd6654ae34200ecec9a439ff91
friedrichOsDev <f39597054@gmail.com>
2026-01-06 14:42:39 +0100

    Refactor kernel code: remove framebuffer drawing logic and add console and framebuffer driver headers

---
+++ src/kernel/include/console.h
@@ -0,0 +1,7 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#endif // CONSOLE_H
---
+++ src/kernel/include/fb.h
@@ -0,0 +1,7 @@
+#ifndef FB_H
+#define FB_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#endif // FB_H
