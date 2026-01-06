4f87c3faaaa4b2ecf5f3934ce538c47ee85a680e
friedrichOsDev <f39597054@gmail.com>
2026-01-06 19:23:06 +0100

    Implement I/O functions: add outb, inb, outw, inw, outl, and inl for port communication

---
+++ src/kernel/lib/io.c
@@ -0,0 +1,37 @@
+#include "../include/io.h"
+
+// write one byte to the bus
+void outb(uint16_t port, uint8_t val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// read one byte from the bus
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+// write two bytes from the bus
+void outw(uint16_t port, uint16_t val) {
+    asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// read two bytes from the bus
+uint16_t inw(uint16_t port) {
+    uint16_t ret;
+    asm volatile ("inw %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+// write four bytes to the bus
+void outl(uint16_t port, uint32_t val) {
+    asm volatile ("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// read four bytes from the bus
+uint32_t inl(uint16_t port) {
+    uint32_t ret;
+    asm volatile ("inl %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
