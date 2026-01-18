f19a5eee697df42b1952625d64d202bf8ed45808
friedrichOsDev <f39597054@gmail.com>
2026-01-18 18:52:14 +0000

    Add initial paging header and implementation files

---
+++ src/kernel/include/paging.h
@@ -0,0 +1,19 @@
+#ifndef PAGING_H
+#define PAGING_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define PAGING_PAGE_SIZE 4096
+#define PAGING_TOTAL_ENTRIES_PER_TABLE 1024
+
+#define PAGE_DIRECTORY_MASK 0xFFFFF000
+
+#define PAGING_CACHE_DISABLED  0b00010000
+#define PAGING_WRITE_THROUGH   0b00001000
+#define PAGING_ACCESS_FROM_ALL 0b00000100
+#define PAGING_IS_WRITABLE     0b00000010
+#define PAGING_IS_PRESENT      0b00000001
+
+#endif // PAGING_H
