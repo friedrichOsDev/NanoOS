776ae3004c8a3ea089a001c3a525874e0ea56bf1
friedrichOsDev <f39597054@gmail.com>
2026-02-16 14:30:36 +0000

    Implement multiboot parsing

--- src/kernel/core/kernel.c
+++ src/kernel/core/kernel.c
@@ -10,14 +10,15 @@
 #include <idt.h>
 #include <irq.h>
 
+mmap_t kernel_mmap;
+fb_info_t kernel_fb_info;
+
 /*
- * Kernel entry point
+ * Parse the multiboot information structure provided by the bootloader
  * @param multiboot_magic The magic number passed by the bootloader
  * @param multiboot_info The address of the multiboot information structure
  */
-void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info) {
-    serial_init();
-
+void multiboot_parse(uint32_t multiboot_magic, uint32_t multiboot_info) {
     if (multiboot_info == 0) {
         serial_printf("Error: Multiboot Info structure is NULL! Expected a pointer to the multiboot structure\n");
         while (1);
@@ -28,10 +29,77 @@ void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info) {
         while (1);
     }
 
+    multiboot_info_t* mbi = (multiboot_info_t*)multiboot_info;
+    serial_printf("Multiboot: Info at %x with size %d\n", multiboot_info, mbi->total_size);
+
+    multiboot_tag_t* tag = (multiboot_tag_t*)(multiboot_info + sizeof(multiboot_info_t));
+
+    while (tag->type != MULTIBOOT_TAG_TYPE_END) {
+        serial_printf("Multiboot: Tag type: %d, size: %d\n", tag->type, tag->size);
+        switch (tag->type) {
+            case MULTIBOOT_TAG_TYPE_CMDLINE:
+                multiboot_tag_cmdline_t* cmdline_tag = (multiboot_tag_cmdline_t*)tag;
+                if (cmdline_tag->size > 8 && cmdline_tag->string[0] != '\0') {
+                    serial_printf("Multiboot: Command line: '%s'\n", cmdline_tag->string);
+                } else {
+                    serial_printf("Multiboot: Command line: (empty)\n");
+                }
+                break;
+            case MULTIBOOT_TAG_TYPE_BOOT_LOADER:
+                multiboot_tag_boot_loader_t* boot_loader_tag = (multiboot_tag_boot_loader_t*)tag;
+                serial_printf("Multiboot: Boot loader name: %s\n", boot_loader_tag->string);
+                break;
+            case MULTIBOOT_TAG_TYPE_MMAP:
+                multiboot_tag_mmap_t* mmap_tag = (multiboot_tag_mmap_t*)tag;
+                kernel_mmap.entry_count = 0;
+                for (multiboot_tag_mmap_entry_t* entry = mmap_tag->entries; (uint8_t*)entry < (uint8_t*)tag + tag->size; entry = (multiboot_tag_mmap_entry_t*)((uintptr_t)entry + mmap_tag->entry_size)) {
+                    if (kernel_mmap.entry_count < MMAP_MAX_ENTRIES) {
+                        kernel_mmap.entries[kernel_mmap.entry_count].base_addr = entry->base_addr;
+                        kernel_mmap.entries[kernel_mmap.entry_count].length = entry->length;
+                        kernel_mmap.entries[kernel_mmap.entry_count].type = (mmap_type_t)entry->type;
+                        kernel_mmap.entry_count++;
+                    }
+                    serial_printf("Multiboot: Memory region: base=%x:%x, len=%x:%x, type=%d\n",
+                        (uint32_t)(entry->base_addr >> 32), (uint32_t)entry->base_addr,
+                        (uint32_t)(entry->length >> 32), (uint32_t)entry->length,
+                        entry->type);
+                }
+                break;
+            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
+                multiboot_tag_framebuffer_t* fb_tag = (multiboot_tag_framebuffer_t*)tag;
+                kernel_fb_info.fb_addr = (void*)(uintptr_t)fb_tag->framebuffer_addr;
+                kernel_fb_info.fb_width = fb_tag->framebuffer_width;
+                kernel_fb_info.fb_height = fb_tag->framebuffer_height;
+                kernel_fb_info.fb_pitch = fb_tag->framebuffer_pitch;
+                kernel_fb_info.fb_bpp = fb_tag->framebuffer_bpp;
+                
+                serial_printf("Multiboot: Framebuffer: %dx%dx%d at %x, type: %d\n", 
+                    fb_tag->framebuffer_width, fb_tag->framebuffer_height, fb_tag->framebuffer_bpp, 
+                    (uint32_t)fb_tag->framebuffer_addr, fb_tag->framebuffer_type);
+                
+                if (fb_tag->framebuffer_type == 2) {
+                    serial_printf("Multiboot: Warning: Framebuffer is in TEXT MODE (0xB8000). Update Multiboot Header!\n");
+                }
+                break;
+            default: break;
+        }
+        tag = (multiboot_tag_t*)((uint8_t*)tag + ((tag->size + 7) & ~7));
+    }
+}
+
+/*
+ * Kernel entry point
+ * @param multiboot_magic The magic number passed by the bootloader
+ * @param multiboot_info The address of the multiboot information structure
+ */
+void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info) {
+    serial_init();
     gdt_init();
     idt_init();
     irq_init();
     idt_enable();
+
+    multiboot_parse(multiboot_magic, multiboot_info);
 
     serial_printf("Kernel: Welcome to NanoOS!\n");
 
--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -7,3 +7,118 @@
 #pragma once
 
 #include <stdint.h>
+
+#define MULTIBOOT_TAG_TYPE_END 0
+#define MULTIBOOT_TAG_TYPE_CMDLINE 1
+#define MULTIBOOT_TAG_TYPE_BOOT_LOADER 2
+#define MULTIBOOT_TAG_TYPE_MMAP 6
+#define MULTIBOOT_TAG_TYPE_FRAMEBUFFER 8
+
+#define MMAP_MAX_ENTRIES 128
+
+typedef struct {
+    uint32_t total_size;
+    uint32_t reserved;
+} __attribute__((packed)) multiboot_info_t;
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+} __attribute__((packed)) multiboot_tag_t;
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+    char string[];
+} __attribute__((packed)) multiboot_tag_cmdline_t;
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+    char string[];
+} __attribute__((packed)) multiboot_tag_boot_loader_t;
+
+typedef struct {
+    uint64_t base_addr;
+    uint64_t length;
+    uint32_t type;
+    uint32_t reserved;
+} __attribute__((packed)) multiboot_tag_mmap_entry_t;
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+    uint32_t entry_size;
+    uint32_t entry_version;
+    multiboot_tag_mmap_entry_t entries[];
+} __attribute__((packed)) multiboot_tag_mmap_t;
+
+
+// FB-TYPE = 0
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} __attribute__((packed)) multiboot_tag_framebuffer_palette_t;
+
+typedef struct {
+    uint32_t framebuffer_palette_num_colors;
+    multiboot_tag_framebuffer_palette_t framebuffer_palette[];
+} __attribute__((packed)) multiboot_tag_framebuffer_color_info_t;
+
+// FB-TYPE = 1
+typedef struct {
+    uint8_t framebuffer_red_field_position;
+    uint8_t framebuffer_red_mask_size;
+    uint8_t framebuffer_green_field_position;
+    uint8_t framebuffer_green_mask_size;
+    uint8_t framebuffer_blue_field_position;
+    uint8_t framebuffer_blue_mask_size;
+} __attribute__((packed)) multiboot_tag_framebuffer_rgb_info_t;
+
+// FB-TYPE = 2
+// --> no additional color info
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t framebuffer_bpp;
+    uint8_t framebuffer_type;
+    uint16_t reserved;
+    uint8_t framebuffer_data[];
+} __attribute__((packed)) multiboot_tag_framebuffer_t;
+
+// simpler structures
+typedef struct {
+    void* fb_addr;
+    uint32_t fb_width;
+    uint32_t fb_height;
+    uint32_t fb_pitch;
+    uint8_t fb_bpp;
+} fb_info_t;
+
+typedef enum {
+    MMAP_USABLE = 1,
+    MMAP_RESERVED = 2,
+    MMAP_ACPI_RECLAIMABLE = 3,
+    MMAP_NVS = 4,
+    MMAP_BADRAM = 5
+} mmap_type_t;
+
+typedef struct {
+    uint64_t base_addr;
+    uint64_t length;
+    mmap_type_t type;
+} mmap_entry_t;
+
+typedef struct {
+    uint32_t entry_count;
+    mmap_entry_t entries[MMAP_MAX_ENTRIES];
+} mmap_t;
+
+extern mmap_t kernel_mmap;
+extern fb_info_t kernel_fb_info;
