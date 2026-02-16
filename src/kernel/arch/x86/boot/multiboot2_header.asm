1e466e74b0ad3b81dc3907a2e55f125643f54632
friedrichOsDev <f39597054@gmail.com>
2026-02-16 14:28:24 +0000

    Update GRUB configuration and refactor multiboot2 header for clarity

--- src/kernel/arch/x86/boot/multiboot2_header.asm
+++ src/kernel/arch/x86/boot/multiboot2_header.asm
@@ -1,14 +1,29 @@
 [BITS 32]
 
+MAGIC equ 0xE85250D6
+ARCH  equ 0 ; protected mode i386
+
 section .multiboot
 align 8
 global multiboot2_header
 
 multiboot2_header:
-    dd 0xE85250D6 ; magic
-    dd 0 ; architecture (0 = i386)
+    dd MAGIC
+    dd ARCH
     dd header_end - multiboot2_header
-    dd - (0xE85250D6 + 0 + (header_end - multiboot2_header))
+    dd 0x100000000 - (MAGIC + ARCH + (header_end - multiboot2_header))
+
+    ; Framebuffer Tag
+    align 8
+    dw 5 ; type
+    dw 0 ; flags
+    dd 20 ; size
+    dd 0 ; width
+    dd 0 ; height
+    dd 0 ; depth
+
+    ; End Tag
+    align 8
     dw 0 ; type
     dw 0 ; flags
     dd 8 ; size
