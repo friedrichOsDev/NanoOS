d8a35ac6365ef81500f7ee7f091b17f3ea380f66
friedrichOsDev <f39597054@gmail.com>
2026-03-09 15:27:05 +0000

    add blinking cursor and update handling

--- src/kernel/drivers/video/console.c
+++ src/kernel/drivers/video/console.c
@@ -20,9 +20,17 @@ static font_color_t default_color;
 static font_color_t console_color;
 static console_buffer_t console_buffer;
 static bool update_flag = false;
+static bool cursor_visible = true;
+static uint32_t cursor_unicode = U'_';
+static uint32_t cursor_old_x = 0;
+static uint32_t cursor_old_y = 0;
 
 static void console_update_event(void) {
     update_flag = true;
+}
+
+static void console_cursor_blink_event(void) {
+    cursor_visible = !cursor_visible;
 }
 
 /**
@@ -30,9 +38,34 @@ static void console_update_event(void) {
  * Should be called in the main kernel loop.
  */
 void console_update(void) {
+    if (cursor_old_x != console_x || cursor_old_y != console_y) {
+        uint32_t buffer_size = console_buffer.width * console_buffer.height;
+        uint32_t offset = CONSOLE_XY_TO_IDX(cursor_old_x, cursor_old_y, console_buffer.width);
+        uint32_t idx = (console_buffer.head + offset) % buffer_size;
+        console_buffer.buffer[idx] |= CONSOLE_BUFFER_DIRTY_BIT;
+
+        cursor_old_x = console_x;
+        cursor_old_y = console_y;
+    }
+
     if (update_flag) {
         console_draw_buffers();
         update_flag = false;
+    }
+
+    // Draw cursor
+    if (cursor_visible) {
+        uint32_t x = console_x * font_get_width() + console_x_start;
+        uint32_t y = console_y * font_get_height() + console_y_start;
+        fb_draw_unicode(cursor_unicode, x, y, console_color.fg_color, console_color.bg_color);
+    } else {
+        uint32_t x = console_x * font_get_width() + console_x_start;
+        uint32_t y = console_y * font_get_height() + console_y_start;
+        uint32_t buffer_size = console_buffer.width * console_buffer.height;
+        uint32_t offset = CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+        uint32_t idx = (console_buffer.head + offset) % buffer_size;
+        uint32_t unicode = console_buffer.buffer[idx] & ~CONSOLE_BUFFER_DIRTY_BIT;
+        fb_draw_unicode(unicode, x, y, console_color.fg_color, console_color.bg_color);
     }
 }
 
@@ -62,9 +95,19 @@ void console_init(uint32_t x, uint32_t y, uint32_t w, uint32_t h, font_color_t c
         .repeat = true,
         .active = true
     };
+    event_t cursor_blink_event = {
+        .event_id = 0,
+        .handler = console_cursor_blink_event,
+        .interval = TIMER_FREQUENCY / 2,
+        .target_tick = timer_get_ticks() + TIMER_FREQUENCY / 2,
+        .repeat = true,
+        .active = true
+    };
 
     uint32_t update_event_id = timer_add_event(update_event);
+    uint32_t cursor_blink_event_id = timer_add_event(cursor_blink_event);
     (void)update_event_id;
+    (void)cursor_blink_event_id;
 
     serial_printf("Console: done\n");
 
@@ -106,6 +149,7 @@ void console_set_window(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
  * @param unicode The Unicode character to print.
  */
 void console_putc(uint32_t unicode) {
+    uint32_t buffer_size = console_buffer.width * console_buffer.height;
     switch (unicode) {
         case U'\n': {
             console_x = 0;
@@ -122,19 +166,25 @@ void console_putc(uint32_t unicode) {
             } else if (console_y > 0) {
                 console_y--;
             
-                uint32_t line_idx = console_buffer.head + CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
-                for (uint32_t i = console_buffer.width - 1; i > 0; i--) {
-                    if ((console_buffer.buffer[line_idx + i] & ~CONSOLE_BUFFER_DIRTY_BIT) != U' ') {
+                uint32_t line_start_offset = CONSOLE_XY_TO_IDX(0, console_y, console_buffer.width);
+                console_x = 0;
+                uint32_t i = console_buffer.width;
+                while (i > 0) {
+                    i--;
+                    uint32_t check_idx = (console_buffer.head + line_start_offset + i) % buffer_size;
+                    if ((console_buffer.buffer[check_idx] & ~CONSOLE_BUFFER_DIRTY_BIT) != U' ') {
                         console_x = i;
                         break;
                     }
                 }
 
-                uint32_t idx = console_buffer.head + CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+                uint32_t offset = CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+                uint32_t idx = (console_buffer.head + offset) % buffer_size;
                 console_buffer.buffer[idx] = U' ' | CONSOLE_BUFFER_DIRTY_BIT;
                 break;
             }
-            uint32_t idx = console_buffer.head + CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+            uint32_t offset = CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+            uint32_t idx = (console_buffer.head + offset) % buffer_size;
             console_buffer.buffer[idx] = U' ' | CONSOLE_BUFFER_DIRTY_BIT;
             break;
         }
@@ -143,7 +193,8 @@ void console_putc(uint32_t unicode) {
             break;
         }
         default: {
-            uint32_t idx = console_buffer.head + CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+            uint32_t offset = CONSOLE_XY_TO_IDX(console_x, console_y, console_buffer.width);
+            uint32_t idx = (console_buffer.head + offset) % buffer_size;
             console_buffer.buffer[idx] = unicode | CONSOLE_BUFFER_DIRTY_BIT;
             console_x++;
             if (console_x >= console_buffer.width) {
@@ -201,10 +252,14 @@ void console_clear(void) {
 }
 
 void console_scroll(void) {
+    uint32_t line_to_clear = console_buffer.head;
     console_buffer.head = (console_buffer.head + console_buffer.width) % (console_buffer.width * console_buffer.height);
-    uint32_t line_idx = console_buffer.head - console_buffer.width;
     for (uint32_t i = 0; i < console_buffer.width; i++) {
-        console_buffer.buffer[line_idx + i] = U' ' | CONSOLE_BUFFER_DIRTY_BIT;
+        console_buffer.buffer[line_to_clear + i] = U' ' | CONSOLE_BUFFER_DIRTY_BIT;
+    }
+
+    for (uint32_t i = 0; i < console_buffer.width * console_buffer.height; i++) {
+        console_buffer.buffer[i] |= CONSOLE_BUFFER_DIRTY_BIT;
     }
 }
 
