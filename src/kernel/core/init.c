076bd100e5ae19bf7beaf2cbe3a7088271c25676
friedrichOsDev <f39597054@gmail.com>
2026-07-09 04:22:20 +0200

    serial_print the multiboot2 info addr und magic

--- src/kernel/core/init.c
+++ src/kernel/core/init.c
@@ -8,11 +8,9 @@
 #include <arch/x86_64/drivers/serial.h>
 
 void kernel_init(const uint64_t magic, const uint64_t info_ptr) {
-    (void)magic;
-    (void)info_ptr;
-
     serial_init(COM1);
     serial_printf(COM1, "Hello From 64-Bit Kernel!\n");
+    serial_printf(COM1, "Multiboot2: Info Ptr: %p\n            Magic: %x\n", info_ptr, magic);
 
     while (1) __asm__("hlt");
 }
