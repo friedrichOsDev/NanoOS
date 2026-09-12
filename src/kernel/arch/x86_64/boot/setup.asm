6c638631d9984a2c009e9733101dc8a1fefb5680
friedrichOsDev <f39597054@gmail.com>
2026-09-12 10:40:28 +0000

    Clean: setup.asm, smb_trampoline.asm

--- src/kernel/arch/x86_64/boot/setup.asm
+++ src/kernel/arch/x86_64/boot/setup.asm
@@ -1,229 +1,208 @@
-    [BITS 32]
-    section .setup
-    global _setup
-    global multiboot_info_ptr
-    global multiboot_magic
-    extern _entry
+        [BITS 32]
+        section .setup
 
-    KERNEL_VIRT_BASE equ 0xFFFFFFFF80000000
-    PML4_INDEX equ (KERNEL_VIRT_BASE >> 39) & 0x1FF ; 511
-    PDPT_INDEX equ (KERNEL_VIRT_BASE >> 30) & 0x1FF ; 510
+        global _setup
+        global multiboot_info_ptr
+        global multiboot_magic
+        extern _entry
+
+        KERNEL_VIRT_BASE equ 0xFFFFFFFF80000000
+        PML4_INDEX equ (KERNEL_VIRT_BASE >> 39) & 0x1FF ; 511
+        PDPT_INDEX equ (KERNEL_VIRT_BASE >> 30) & 0x1FF ; 510
 
 _setup:
-; Save GRUB Multiboot2 address and magic
-    mov [multiboot_info_ptr], ebx
-    mov [multiboot_magic], eax
+        cli
+        cld
 
-; 0. support checks
-    call check_cpuid
-    cmp eax, 1
-    jne .no_long_mode
+; 0. Save Multiboot Parameters & HW Checks
+        mov [multiboot_info_ptr], ebx
+        mov [multiboot_magic], eax
 
-    call check_long_mode_support
-    cmp eax, 1
-    jne .no_long_mode
+        call check_cpuid
+        test eax, eax
+        jz .no_long_mode
 
-; 1. setup page tables
-    mov eax, boot_pdpt
-    or eax, 0x3
+        call check_long_mode_support
+        test eax, eax
+        jz .no_long_mode
 
-    mov [boot_pml4], eax
-    mov dword [boot_pml4 + 4], 0
+; 1. Setup Page Tables
+        mov eax, boot_pdpt
+        or eax, 0x3
+        mov [boot_pml4], eax
+        mov dword [boot_pml4 + 4], 0
+        mov [boot_pml4 + PML4_INDEX * 8], eax
+        mov dword [boot_pml4 + PML4_INDEX * 8 + 4], 0
 
-    mov [boot_pml4 + PML4_INDEX * 8], eax
-    mov dword [boot_pml4 + PML4_INDEX * 8 + 4], 0
+        mov eax, boot_pdpt_direct
+        or eax, 0x3
+        mov [boot_pml4 + 256 * 8], eax
+        mov dword [boot_pml4 + 256 * 8 + 4], 0
 
-    mov eax, boot_pdpt_direct
-    or eax, 0x3
+        mov eax, boot_pd_low
+        or eax, 0x3
+        mov [boot_pdpt], eax
+        mov dword [boot_pdpt + 4], 0
+        mov [boot_pdpt + PDPT_INDEX * 8], eax
+        mov dword [boot_pdpt + PDPT_INDEX * 8 + 4], 0
 
-    mov [boot_pml4 + 256 * 8], eax
-    mov dword [boot_pml4 + 256 * 8 + 4], 0
-
-    mov eax, boot_pd_low
-    or eax, 0x3
-
-    mov [boot_pdpt], eax
-    mov dword [boot_pdpt + 4], 0
-
-    mov [boot_pdpt + PDPT_INDEX * 8], eax
-    mov dword [boot_pdpt + PDPT_INDEX * 8 + 4], 0
-
-    mov edi, boot_pdpt_direct
-    mov ecx, 4
-    mov eax, boot_pd_high
-    or eax, 0x3
+        mov edi, boot_pdpt_direct
+        mov ecx, 4
+        mov eax, boot_pd_high
+        or eax, 0x3
 
 .link_direct_pds:
-    mov [edi], eax
-    mov dword [edi + 4], 0
+        mov [edi], eax
+        mov dword [edi + 4], 0
+        add edi, 8
+        add eax, 4096
+        loop .link_direct_pds
 
-    add edi, 8
-    add eax, 4096
-    loop .link_direct_pds
-
-    mov edi, boot_pd_low
-    mov ecx, 8
-    mov eax, 0x83
+        mov edi, boot_pd_low
+        mov ecx, 8
+        mov eax, 0x83
 
 .fill_kernel_low_pages:
-    mov [edi], eax
-    mov dword [edi + 4], 0
-    add edi, 8
-    add eax, 0x200000
-    loop .fill_kernel_low_pages
+        mov [edi], eax
+        mov dword [edi + 4], 0
+        add edi, 8
+        add eax, 0x200000
+        loop .fill_kernel_low_pages
 
-    mov edi, boot_pd_high
-    mov ecx, 2048
-    mov eax, 0x83
-    mov edx, 0
+        mov edi, boot_pd_high
+        mov ecx, 2048
+        mov eax, 0x83
+        mov edx, 0
 
 .fill_direct_mapping:
-    mov [edi], eax
-    mov [edi + 4], edx
+        mov [edi], eax
+        mov [edi + 4], edx
+        add edi, 8
+        add eax, 0x200000
+        adc edx, 0
+        loop .fill_direct_mapping
 
-    add edi, 8
+; 2. Control Registers & Long Mode Enable
+; Activate PAE
+        mov eax, cr4
+        or eax, 1 << 5
+        mov cr4, eax
 
-    add eax, 0x200000
-    adc edx, 0
+; Load CR3
+        mov eax, boot_pml4
+        mov cr3, eax
 
-    loop .fill_direct_mapping
+; Activate Long Mode & NX-Bit in EFER MSR
+        mov ecx, 0xC0000080
+        rdmsr
+        or eax, (1 << 8) | (1 << 11)
+        wrmsr
 
-; 2. activate PAE
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
+; Enable Paging
+        mov eax, cr0
+        or eax, (1 << 31) | (1 << 16)
+        mov cr0, eax
 
-; 3. load PML4 in CR3
-    mov eax, boot_pml4
-    mov cr3, eax
+; Load 64-Bit GDT
+        lgdt [gdt64_ptr]
 
-; 4. activate Long Mode in EFER MSR
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-; 5. enable paging
-    mov eax, cr0
-    or eax, 1 << 31                    ; set PG-bit (bit 31)
-    mov cr0, eax
-
-; 6. load 64-bit gdt
-    lgdt [gdt64_pointer]
-
-; 7. jump to 64-bit
-    jmp 0x08:init_long_mode
+; Far Jump to Long Mode
+        jmp 0x08:init_long_mode
 
 .no_long_mode:
-    hlt
-    jmp $
-
-; From OSDev Wiki
+        cli
+.hang:
+        hlt
+        jmp .hang
 
 check_cpuid:
-    pushfd
-    pop eax
-    mov ecx, eax
-    xor eax, 1 << 21
-    push eax
-    popfd
-    pushfd
-    pop eax
-    push ecx
-    popfd
-    xor eax, ecx
-    jnz .supported
-    mov eax, 0
-    ret
-
-.supported:
-    mov eax, 1
-    ret
-
-; From OSDev Wiki
+        pushfd
+        pop eax
+        mov ecx, eax
+        xor eax, 1 << 21
+        push eax
+        popfd
+        pushfd
+        pop eax
+        push ecx
+        popfd
+        xor eax, ecx
+        jz .not_supported
+        mov eax, 1
+        ret
+.not_supported:
+        xor eax, eax
+        ret
 
 check_long_mode_support:
-    mov eax, 0x80000000
-    cpuid
-    cmp eax, 0x80000001
-    jb .not_supported
+        mov eax, 0x80000000
+        cpuid
+        cmp eax, 0x80000001
+        jb .no_lm
 
-    mov eax, 0x80000001
-    cpuid
-    test edx, 1 << 29
-    jz .not_supported
-    mov eax, 1
-    ret
+        mov eax, 0x80000001
+        cpuid
+        test edx, 1 << 29
+        jz .no_lm
+        mov eax, 1
+        ret
+.no_lm:
+        xor eax, eax
+        ret
 
-.not_supported:
-    mov eax, 1
-    ret
-
-    [BITS 64]
+        [BITS 64]
 
 init_long_mode:
-; 8. switch data segments to 64-bit GDT selectors
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+; 3. Long Mode Data Segments & FPU/SSE Init
+        mov ax, 0x10
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
 
-; 9. FPU / SSE Early HW Init
-    mov rax, cr0
-    and rax, ~((1 << 2) | (1 << 3))    ; clear EM, TS
-    or rax, (1 << 1)                   ; set MP
-    mov cr0, rax
+; FPU / SSE Hardware Init
+        mov rax, cr0
+        and rax, ~((1 << 2) | (1 << 3))
+        or rax, (1 << 1)
+        mov cr0, rax
 
-    mov rax, cr4
-    or rax, (1 << 9) | (1 << 10)       ; set OSFXSR, OSXMMEXCPT
-    mov cr4, rax
+        mov rax, cr4
+        or rax, (1 << 9) | (1 << 10)
+        mov cr4, rax
 
-    fninit
+        fninit
 
-    sub rsp, 4
-    mov dword [rsp], 0x1F80
-    ldmxcsr [rsp]
-    add rsp, 4
+        sub rsp, 16
+        mov dword [rsp], 0x1F80
+        ldmxcsr [rsp]
+        add rsp, 16
 
-; 10. jump to the higher half
-    mov rax, _entry
-    jmp rax
+; 4. Jump to Kernel Entry
+        and rsp, -16
+        mov rax, _entry
+        jmp rax
 
-.hang:
-    cli
-    hlt
-    jmp .hang
+        align 8
+gdt64_start:
+        dq 0x0000000000000000          ; 0x00: Null
+        dq 0x00209A0000000000          ; 0x18: 64-Bit Code
+        dq 0x0000920000000000          ; 0x20: 64-Bit Data
+gdt64_end:
 
-; 64-bit gdt
-    align 8
+gdt64_ptr:
+        dw gdt64_end - gdt64_start - 1
+        dq gdt64_start
 
-gdt64:
-.Null:
-    dq 0                               ; null
-
-.Code:
-    dq 0x00209A0000000000              ; code: Long Mode + Present + Exec/Read
-
-.Data:
-    dq 0x0000920000000000              ; data: Present + Writable
-
-gdt64_pointer:
-    dw $ - gdt64 - 1
-    dq dword gdt64
-
-; multiboot2 info
-    align 8
+        align 8
 multiboot_info_ptr:
-    dq 0
+        dq 0
 multiboot_magic:
-    dq 0
+        dq 0
 
-; page tables
-    align 4096
-    boot_pml4: times 4096 db 0
-    boot_pdpt: times 4096 db 0
-    boot_pdpt_direct: times 4096 db 0
-    boot_pd_low: times 4096 db 0
-    boot_pd_high: times 4096 * 4 db 0
+        align 4096
+        boot_pml4: times 4096 db 0
+        boot_pdpt: times 4096 db 0
+        boot_pdpt_direct: times 4096 db 0
+        boot_pd_low: times 4096 db 0
+        boot_pd_high: times 4096 * 4 db 0
